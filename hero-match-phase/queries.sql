e17ad394b3f3829a91cc1cdb58df66068b840af1
Drew Harry <drew.harry@gmail.com>
2013-02-14 16:53:52 -0500

    Got far enough with sql, switching to lasers (javascript)

--- hero-match-phase/queries.sql
+++ hero-match-phase/queries.sql
@@ -11,3 +11,7 @@ select avg(match_time), max(match_time), min(match_time), stddev(match_time) fro
   -- I'm not sure how index and winner are mapped; it could be backwards. 
   -- check with martin.
   select mymatch.id as matchid, bs_info.index, hero_name, match_time, (bs_info.index < 5 && winner=2) or (bs_info.index >= 5 && winner=3) as winner from bs_info join herodisp on bs_info.hero_num = herodisp.hero_num join mymatch on mymatch.id=bs_info.id join basic_stats2 on basic_stats2.id=bs_info.id and basic_stats2.index=bs_info.index limit 100;
+
+
+ -- this one does no group-by, just dumps a row for every hero for every game with game length and winner status. having trouble doing group-by, so going to just switch to javascript at this point.
+select mymatch.id as matchid, bs_info.index,gpm, hero_name, match_time, (bs_info.index < 5 && winner=2) or (bs_info.index >= 5 && winner=3) as winner into outfile '/tmp/hero_performance.csv' FIELDS TERMINATED BY ','LINES TERMINATED BY '\n' from bs_info join herodisp on bs_info.hero_num = herodisp.hero_num join mymatch on mymatch.id=bs_info.id join basic_stats2 on basic_stats2.id=bs_info.id and basic_stats2.index=bs_info.index;
